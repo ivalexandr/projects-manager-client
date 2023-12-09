ee8ae194428c438732152976268941fad28da5ff
Alexandr Ivanin <ya@aivanin.ru>
2023-12-09 18:45:32 +0300

    feat(#PMP-23, #PMP-27): added top bar layout

---
+++ src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { AsyncPipe, UpperCasePipe } from '@angular/common';
+import { AuthDialogService } from '../auth-dialog/auth-dialog.service';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { Store, select } from '@ngrx/store';
+import { TAppStore } from '../../app.config';
+import { selectAuthUser } from '../../store/auth/auth.selectors';
+import * as authActions from '../../store/auth/auth.actions';
+
+@Component({
+  selector: 'app-top-bar',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    UpperCasePipe,
+    AsyncPipe,
+    MatMenuModule,
+    MatIconModule,
+  ],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.scss',
+})
+export class TopBarComponent {
+  authUser$ = this.store.pipe(select(selectAuthUser));
+
+  componentText = {
+    button: 'Войти',
+    title: 'Projects-manager',
+    menu: {
+      profile: 'Профиль',
+      settings: 'Настройки',
+      exit: 'Выход',
+    },
+  };
+
+  constructor(
+    private readonly authDialogService: AuthDialogService,
+    private readonly store: Store<TAppStore>
+  ) {}
+
+  clickButtonHandler() {
+    this.authDialogService.openDialog();
+  }
+
+  logoutHandler() {
+    this.store.dispatch(authActions.logout());
+  }
+}
