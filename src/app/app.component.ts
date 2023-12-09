df87aea66a305cbdc6e7ede5d06bc2376515b159
Alexandr Ivanin <ya@aivanin.ru>
2023-12-09 18:46:46 +0300

    feat(#PMP-23, #PMP-27): app config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { Store, select } from '@ngrx/store';
+import { selectErrorAuth } from './store/auth/auth.selectors';
+import { TAppStore } from './app.config';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { resetError } from './store/auth/auth.actions';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
   title = 'projects-manager-client';
+  authError$ = this.store.pipe(select(selectErrorAuth));
+
+  constructor(
+    private readonly store: Store<TAppStore>,
+    private readonly snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    this.authError$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((error) => {
+        if (error) {
+          this.snackBar.open(error, 'Закрыть', { duration: 5000 });
+          this.store.dispatch(resetError());
+        }
+      });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,40 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { graphqlProvider } from './graphql.provider';
-import { provideStore } from '@ngrx/store';
+import { ActionReducerMap, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { TAuthReducer, authReducer } from './store/auth/auth.reducer';
+import { AuthEffects } from './store/auth/auth.effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { AppInitService } from './services/app-init.service';
+
+export type TAppStore = {
+  auth: TAuthReducer;
+};
+
+const reducers: ActionReducerMap<TAppStore> = {
+  auth: authReducer,
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(), graphqlProvider, provideStore(), provideEffects()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    graphqlProvider,
+    provideStore<TAppStore>(reducers),
+    provideEffects(AuthEffects),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (appInitService: AppInitService) => () =>
+        appInitService.init(),
+      deps: [AppInitService],
+      multi: true,
+    },
+  ],
 };
