d4bb1bfd3b9a417bac1bd67b06292f4b7831bbc8
Alexandr Ivanin <ya@aivanin.ru>
2023-12-22 23:30:26 +0300

    change: main layout

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TopBarComponent } from '../../components/top-bar/top-bar.component';
+import { LeftPanelComponent } from '../../components/left-panel/left-panel.component';
+import { CenterPanelComponent } from '../../components/center-panel/center-panel.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [TopBarComponent],
+  imports: [TopBarComponent, LeftPanelComponent, CenterPanelComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  isHideSidebar = signal(false);
+
+  hideSideBarHandler() {
+    this.isHideSidebar.update(state => !state);
+  }
+}
